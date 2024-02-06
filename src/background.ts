90c1186cd2c1e831dddca56bc50096fb8434c689
Mark Wetzel <mark@markwetzel.com>
2024-02-06 16:17:34 -0500

    Changes to make buildable

--- src/background.ts
+++ src/background.ts
@@ -2,21 +2,47 @@ import * as browser from "webextension-polyfill";
 
 console.log("Extension loaded");
 
-function filterHistory(): void {
-  const blockedKeywords: string[] = ["facebook", "youtube", "billionaires"];
-  const blockedDomains: string[] = ["example.com", "sub.example.com"];
+// I want a function that will store sample blocked keywords in local storage (if the user hasn't already set them)
+async function getBlockedKeywords(): Promise<string[]> {
+  const blockedKeywords = await browser.storage.local.get("blockedKeywords");
+  if (blockedKeywords.blockedKeywords) {
+    return blockedKeywords.blockedKeywords;
+  } else {
+    await browser.storage.local.set({ blockedKeywords: [] });
+    return [];
+  }
+}
+
+// I want a function that will store sample blocked domains in local storage (if the user hasn't already set them)
+async function getBlockedDomains(): Promise<string[]> {
+  const blockedDomains = await browser.storage.local.get("blockedDomains");
+  if (blockedDomains.blockedDomains) {
+    return blockedDomains.blockedDomains;
+  } else {
+    await browser.storage.local.set({
+      blockedDomains: ["reddit.com/r/gonewild", "reddit.com/r/superstonk"],
+    });
+    return [];
+  }
+}
+
+filterHistory();
+
+async function filterHistory(): Promise<void> {
+  const blockedKeywords = await getBlockedKeywords();
+  const blockedDomains = await getBlockedDomains();
 
   browser.history.search({ text: "", startTime: 0 }).then((historyItems) => {
+    console.log(`Found ${historyItems.length} history items`);
     historyItems.forEach((item) => {
       if (item.url) {
-        const shouldDelete: boolean =
-          blockedKeywords.some(
-            (keyword) =>
-              item.title?.includes(keyword) || item.url?.includes(keyword)
-          ) ||
-          blockedDomains.some((domain) => new URL(item.url!).hostname === domain);
+        // check to see if the current history item contains any part for each blocked domain
+        const shouldDelete = blockedDomains.some((blockedDomain) => {
+          return item.url?.toLowerCase().includes(blockedDomain.toLowerCase());
+        });
 
         if (shouldDelete) {
+          console.log(`Deleting history item: ${item.url}`);
           browser.history.deleteUrl({ url: item.url });
         }
       }
@@ -24,5 +50,6 @@ function filterHistory(): void {
   });
 }
 
-setInterval(filterHistory, 10000);
-console.log("filterHistory will run every 10 seconds");
+const intervalInSeconds = 5;
+setInterval(filterHistory, intervalInSeconds * 1000);
+console.log(`filterHistory will run every ${intervalInSeconds} seconds`);
